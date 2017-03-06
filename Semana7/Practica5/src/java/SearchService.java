5d06678126b5cef554e62298043eb27676b74ded
CesarH-IbarraN <minichamanred@gmail.com>
2017-03-06 11:54:08 -0600

    Add files via upload

---
+++ Semana7/Practica5/src/java/SearchService.java
@@ -0,0 +1,32 @@
+
+import javax.naming.directory.*;
+import javax.security.auth.login.Configuration;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author cesar
+ */
+public class SearchService {
+    private Configuration config;
+    private SearchEngine engine;
+    
+    private SearchEngine createEngine(){
+        return null;
+    }
+
+    /**
+     *
+     * @param query
+     * @return
+     */
+    public SearchResult search(SearchRequest query){
+        return null;
+    }
+    
+}
